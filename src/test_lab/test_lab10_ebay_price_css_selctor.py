1b99f6c3adb46d21844c7c3f785d0000f4b8eaca
bhargav <purohitbharga28699@gmail.com>
2024-12-24 14:49:15 +0530

    yesss

--- src/test_lab/test_lab10_ebay_price_css_selctor.py
+++ src/test_lab/test_lab10_ebay_price_css_selctor.py
@@ -5,3 +5,25 @@ from selenium.webdriver.chrome.options import Options
 import time
 from selenium.webdriver.common.by import By
 
+
+def test_ebay_macmini():
+
+    driver = webdriver.Chrome()
+    driver.get("https://www.ebay.com/")
+
+    search_box = driver.find_element(By.ID, value="gh-ac")
+    search_box.send_keys("macmini")
+    submit = driver.find_element(By.XPATH, "//input[@id='gh-btn']")
+    submit.click()
+
+    item = driver.find_elements(By.CSS_SELECTOR, ".s-item")
+
+    for i in item:
+
+        name = i.find_element(By.CLASS_NAME, "s-item__title").text
+        price = i.find_element(By.CLASS_NAME, "s-item__price").text
+
+        print(f"\nname is {name} , value prove is  .{price} ")
+
+
+    driver.quit()
